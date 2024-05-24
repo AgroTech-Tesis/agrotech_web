695f157ae0a418dd7864ca50889dc4adb6f7343d
LuisLi18 <lrlitang18@gmail.com>
2024-05-24 04:07:07 -0500

    feat: see more information in each IoT device

--- src/app/IrrigationManagement/pages/devices-view/devices-view.component.ts
+++ src/app/IrrigationManagement/pages/devices-view/devices-view.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {DeviceService} from "../../services/device.service";
 import {Device} from "../../model/Device";
 import {HttpClientModule} from "@angular/common/http";
+import {MatAccordion, MatExpansionModule, MatExpansionPanel} from '@angular/material/expansion';
+import {NgForOf, NgIf} from "@angular/common";
+import {provideNativeDateAdapter} from "@angular/material/core";
+import {MatButton} from "@angular/material/button";
+import {MatDivider} from "@angular/material/divider";
+
 
 @Component({
   selector: 'devices-view',
   standalone: true,
-  imports: [MatIconModule, MatTableModule, HttpClientModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatIconModule, MatTableModule, HttpClientModule, MatExpansionModule, NgForOf, NgIf, MatButton, MatDivider],
   templateUrl: './devices-view.component.html',
   styleUrl: './devices-view.component.css'
 })
-export class DevicesViewComponent {
+export class DevicesViewComponent implements OnInit{
   deviceData: MatTableDataSource<Device>
+  @ViewChild(MatAccordion) accordion: MatAccordion | undefined;
+  panelOpenState = false;
+
   // dataSource:any = [
   //   {ubication: "Parcela 1", name: 'Esp32-1', createDate: "20-04-2024 12:19:50", status: 'connected'},
   //   {ubication: "Parcela 2", name: 'Esp32-2', createDate: "20-04-2024 12:19:50", status: 'connected'},
@@ -24,7 +34,6 @@ export class DevicesViewComponent {
   constructor(private deviceService: DeviceService) {
       this.deviceData = {} as MatTableDataSource<Device>
   }
-
   ngOnInit(): void {
     this.getAllDevices();
 
@@ -37,4 +46,10 @@ export class DevicesViewComponent {
     })
 
   }
+
+  formatDateTime(dateTimeString: string): string {
+    const dateTime = new Date(dateTimeString);
+    return dateTime.toLocaleString('es-ES');
+  }
+
 }
