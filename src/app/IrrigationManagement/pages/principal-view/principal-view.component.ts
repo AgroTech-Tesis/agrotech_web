c0db91db80c2b8c3b89ad5bfb73dcf2310674918
LuisLi18 <lrlitang18@gmail.com>
2024-05-24 04:41:12 -0500

    feat: added dynamic bar chart in control panel

--- src/app/IrrigationManagement/pages/principal-view/principal-view.component.ts
+++ src/app/IrrigationManagement/pages/principal-view/principal-view.component.ts
@@ -3,6 +3,8 @@ import {NgIf} from "@angular/common";
 import {MatIconModule} from '@angular/material/icon';
 import {WeatherForecastService} from "../../services/weather-forecast.service";
 import {RouterLink, RouterOutlet} from "@angular/router";
+import {NgxChartsModule, ScaleType} from "@swimlane/ngx-charts";
+import {single} from "../../model/BarChartData";
 
 @Component({
   selector: 'principal-view',
@@ -11,14 +13,20 @@ import {RouterLink, RouterOutlet} from "@angular/router";
     NgIf,
     MatIconModule,
     RouterLink,
-    RouterOutlet
+    RouterOutlet,
+    NgxChartsModule
   ],
   templateUrl: './principal-view.component.html',
   styleUrl: './principal-view.component.css'
 })
 export class PrincipalViewComponent implements OnInit{
   WeatherData: any;
+
   constructor(private weatherForecastService: WeatherForecastService) {
+    Object.assign(this, { single })
+    // Object.assign(this, { single1 });
+
+
   }
 
   ngOnInit() {
@@ -32,4 +40,60 @@ export class PrincipalViewComponent implements OnInit{
       });
   }
 
+  // BAR CHART
+
+  view: [number, number] = [300, 100];
+
+  // options
+  showXAxis = false;
+  showYAxis = false;
+  gradient = false;
+  showLegend = false;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+
+  colorScheme = {
+    name: 'myScheme',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#f00', '#0f0', '#0ff', '#0ff', '#0ff', '#0ff'],
+  };
+
+
+  onSelect(event: any) {
+    console.log(event);
+  }
+
+  protected readonly single = single;
+
+
+  // PIE CHART
+
+  view1: [number, number] = [300, 100];
+
+  // options
+  gradient1: boolean = true;
+  showLegend1: boolean = true;
+  showLabels1: boolean = true;
+  isDoughnut1: boolean = false;
+  legendPosition1: string = 'below';
+
+  colorScheme1 = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+
+  onSelect1(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate1(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate1(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }
