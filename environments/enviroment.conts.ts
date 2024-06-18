d9c4ae060752ee0acb730f60b18d3cb6bca6a685
LuisLi18 <lrlitang18@gmail.com>
2024-06-18 02:45:38 -0500

    fix: return to local

--- environments/enviroment.conts.ts
+++ environments/enviroment.conts.ts
@@ -1,7 +1,7 @@
 export const HOST = {
-    // local: 'http://localhost:8080',
+    local: 'http://localhost:8080',
     local2: 'http://localhost:8081',
-    local: 'http://despliegue.westus3.cloudapp.azure.com'
+    // local: 'http://despliegue.westus3.cloudapp.azure.com'
 };
 
 export const hostSelected = {
